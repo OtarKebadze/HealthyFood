d14d4e3260162a03aa20b57f956327f22bf86998
OtarKebadze <OtarKebadze@gmail.com>
2022-01-12 14:06:34 -0300

    cambios

--- js/index.js
+++ js/index.js
@@ -7,16 +7,16 @@ import {nuestrosProductos} from './productos.js'
 /* Creando carrito vacio*/
 const carrito = new Carrito ();
 
+
 for ( const producto of nuestrosProductos){
-    $('#productos__contenido').append(`<li id="${producto.id}"> ${producto.nombre} </li>`);
+    $('#productos__contenido').append(`<li id="${producto.id}">${producto.nombre} </li>`);
     $(`#${producto.id}`).click(function(){
-    $("#tuCompra").append(`<li id="${producto.id}"> ${producto.nombre}</li>`);
+    ($("#tuCompra").append(`<li>${producto.nombre}</li>`));
     })
 };
 
 $("#botonCompra").click(function(){
     alert (`Gracias por realizar su compra`);
-    alert("vacio la lista");
     });
 
     $("#botonCancela").click(function(){
