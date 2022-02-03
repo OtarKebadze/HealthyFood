0a65da3caec91c03befca4f314118ec6d8ddc20f
OtarKebadze <OtarKebadze@gmail.com>
2022-02-03 17:00:03 -0300

    cambios

--- js/index.js
+++ js/index.js
@@ -16,14 +16,21 @@ for ( let producto of nuestrosProductos){
     $(`#${producto.id}`).click(function(){
     carrito.createItem(producto);
     if( verdadero == false ){
-    ($("#tuCompra").append(`<li>${producto.nombre}<input id="cantidad" type="number" min="1" value=1 onclick="carrito.deleteItem()"><button id="elimina">Eliminar</button>`));
-    verdadero=true;
-    }else{
-    alert(`el item ${producto.nombre} ya existe en el carrito`)
-    }
+    ($("#tuCompra").append(`<li>${producto.nombre}<input id="cantidad" type="number" min="1" value=1><button id="elimina" data-id="${producto.id}">Eliminar</button>`));
+    verdadero = true;
+$('button#elimina').click(function(){
+    verdadero = false;
+    let idBoton = $(this).data('id');
+    carrito.deleteItem(idBoton);
+    $(this).parent().fadeOut(function () {
+    $(this).remove();
+        });
 })
+} else {
+    alert(`el item ${producto.nombre} ya existe en el carrito`)
 }
-/*Botones UP y DOWN Input*/
+});
+}
 
 
 /*Botones*/
@@ -39,13 +46,22 @@ $("#botonCancela").click(function(){
 });
 
 
-for ( let deleted of nuestrosProductos){
-let ind = deleted.id
 $(document).delegate("button#elimina","click",function(){
-carrito.deleteItem(ind)
+    var idBoton =$(this).data('id');
+    carrito.deleteItem(idBoton);
+    $(this).parent().fadeOut(function(){
+        $(this).remove();
+        verdadero=false;
+    })
 });
-}
+
 
 /*PRECIO FINAL*/
-
+for ( let prod of nuestrosProductos){
+    let prodCant= document.getElementById("cantidad")
+    let cantidad=prodCant.value
+    let precio = prod.precio
+    let precioFinal= precio* parseInt(cantidad)
+    $("#totalCompra").append(`<p>$${precioFinal}</p>`)
+}
 
