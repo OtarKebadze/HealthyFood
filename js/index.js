0d8b5209ac4f805f700c0174d93c0b576bb44549
OtarKebadze <OtarKebadze@gmail.com>
2022-02-03 12:14:21 -0300

    cambios

--- js/index.js
+++ js/index.js
@@ -10,16 +10,17 @@ const carrito = new Carrito ();
 
 /* Renderizo la lista*/
 
-
 for ( let producto of nuestrosProductos){
     $('#listaProductos').append(`<li id="${producto.id}" class="productoNombre">${producto.nombre}</li><p id="precioProducto">$${producto.precio}</p>`);
+    let verdadero = producto.cond;
     $(`#${producto.id}`).click(function(){
     carrito.createItem(producto);
-    let nombreEnsalada = document.getElementsByClassName("compra__contenido");
-    if( producto.nombre !== nombreEnsalada.innerText){
+    if( verdadero == false ){
     ($("#tuCompra").append(`<li>${producto.nombre}<input id="cantidad" type="number" value=${producto.cant}><button id="elimina">Eliminar</button>`));
+    verdadero=true;
     }else{
     alert(`el item ${producto.nombre} ya existe en el carrito`)
+
     }
 })
 }
@@ -27,13 +28,14 @@ for ( let producto of nuestrosProductos){
 $("#cantidad").on("keyup",function(){
     alert("hola")
 });
-$("#cantidad").on("keydown",function(){
+$("#cantidad").keydown(function(){
     alert("hola2")
 });
 
 /*Botones*/
 $("#botonCompra").click(function(){
     alert( `Gracias por su Compra!`)
+    localStorage.clear();
     location.reload();
 });
 
@@ -46,4 +48,5 @@ $("#elimina").click(function(){
     alert(`holaaaaaaa`);
 })
 /*PRECIO FINAL*/
+
 
