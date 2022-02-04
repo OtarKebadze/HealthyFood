cb589749d7e96b10cced41f9baa23bf4b07c3bd3
OtarKebadze <OtarKebadze@gmail.com>
2022-02-03 21:27:02 -0300

    cambios

--- js/index.js
+++ js/index.js
@@ -2,38 +2,18 @@ import {Carrito} from "./carrito.js";
 import {Producto} from "./funcionProducto.js";
 import {nuestrosProductos} from './productos.js'
 
-/* Creando carrito vacio*/
-const carrito = new Carrito ();
-
 
 /* Renderizo la lista*/
 
 for ( let producto of nuestrosProductos){
     $('#listaProductos').append(`<li id="${producto.id}" class="productoNombre">${producto.nombre}</li><p id="precioProducto">$${producto.precio}</p>`);
-    let verdadero = producto.cond;
     $(`#${producto.id}`).click(function(){
+    let carrito=new Carrito;
+    console.log(carrito);
     carrito.createItem(producto);
-    if( verdadero == false ){
-    ($("#tuCompra").append(`<li>${producto.nombre}<input id="cantidad" type="number" min="1" value=1><button id="elimina" data-id="${producto.id}">X</button>`));
-    verdadero = true;
-    $('button#elimina').click(function(){
-    verdadero = false;
-    let idBoton = $(this).data('id');
-    carrito.deleteItem(idBoton);
-    $(this).parent().fadeOut(function () {
-    $(this).remove();
-        });
-})
-$('input#cantidad').change(function(){
-    var cantidadProducto =$(this).val()*producto.precio;
-    $("#totalCompra").append(`<p>$${cantidadProducto}</p>`)
-});
-$("input#cantidad").trigger("change")
-} else {
-    alert(`el item ${producto.nombre} ya existe en el carrito`)
-}
-});
-}
+    console.log(carrito)
+    })
+    }
 
 
 /*Botones*/
@@ -49,3 +29,31 @@ $("#botonCancela").click(function(){
 });
 
 
+/*
+verdadero = true;
+    $('button#elimina').click(function(){
+    verdadero = false;
+    let idBoton = $(this).data('id');
+    carrito.deleteItem(idBoton);
+    $(this).parent().fadeOut(function () {
+    $(this).remove();
+        });
+})
+$('input#cantidad').change(function(){
+    var cantidadProducto =$(this).val()*producto.precio;
+    $("#totalCompra").append(`<p>$${cantidadProducto}</p>`)
+});
+$("input#cantidad").trigger("change")
+
+
+
+
+$(`#${producto.id}`).click(function(){
+    carrito.createItem(producto);
+    const prods = JSON.parse(localStorage.getItem('carrito'))
+    if( producto.id == prods.id ){
+    ($("#tuCompra").append(`<li>${producto.nombre}<input id="cantidad" type="number" min="1" value=1><button id="elimina" data-id="${producto.id}">X</button>`));
+    
+} else {
+    alert(`el item ${producto.nombre} ya existe en el carrito`)
+*/
--- js/productos.js
+++ js/productos.js
@@ -1,13 +1,12 @@
 export const nuestrosProductos = [
-    {id:1 , nombre :'ensalada cesar' , precio: 250 ,stock: true, cond:false},
-    {id:2 , nombre:'ensalada mix de hojas verdes' , precio: 180 , stock: true, cond:false},
-    {id:3 , nombre :'ensalada mixta pollo' ,precio: 230 , stock: true, cond:false},
-    {id:4 , nombre :'ensalada mixta con lomito de atun' , precio: 250 , stock: true, cond:false},
-    {id:5 , nombre :'ensalada mixta con camarones', precio: 315 , stock: true, cond:false},
-    {id:6 ,nombre :'ensalada mixta con salmon' , precio: 300 , stock:true, cond:false},
-    {id:7 ,nombre :'ensalada tomate' , precio: 240  , stock: true, cond:false},
-    {id:8 ,nombre :'ensalada palta y mango' ,precio: 300  , stock: true, cond:false},
-    {id:9 ,nombre :'ensalada rusa' , precio: 250 ,stock: true, cond:false},
-    {id:10 ,nombre :'ensalada waldorf' , precio: 290 ,  stock: true, cond:false}
+    {id:1 , nombre :'ensalada cesar' , precio: 250 ,stock: true},
+    {id:2 , nombre:'ensalada mix de hojas verdes' , precio: 180 , stock: true},
+    {id:3 , nombre :'ensalada mixta pollo' ,precio: 230 , stock: true},
+    {id:4 , nombre :'ensalada mixta con lomito de atun' , precio: 250 , stock: true},
+    {id:5 , nombre :'ensalada mixta con camarones', precio: 315 , stock: true},
+    {id:6 ,nombre :'ensalada mixta con salmon' , precio: 300 , stock:true},
+    {id:7 ,nombre :'ensalada tomate' , precio: 240  , stock: true},
+    {id:8 ,nombre :'ensalada palta y mango' ,precio: 300  , stock: true},
+    {id:9 ,nombre :'ensalada rusa' , precio: 250 ,stock: true},
+    {id:10 ,nombre :'ensalada waldorf' , precio: 290 ,  stock: true}
     ]
-    
